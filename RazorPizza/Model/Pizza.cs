ffa444226804fd1a069be9ff4fb84385de62919a
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-05 15:04:41 +0300

    Pizza Model eklendi

---
+++ RazorPizza/Model/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorPizza.Model
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string ImageTitle { get; set; }
+        public float BasePrice { get; set; } = 10;
+        public bool Sauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Pepperoni { get; set; }
+    }
+}
