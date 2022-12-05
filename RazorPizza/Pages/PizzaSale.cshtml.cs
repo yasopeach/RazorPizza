f0bc4db4b6d0f2f7444b2e40a3314be04970356f
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-05 13:25:17 +0300

    sayfalar ve linkler eklendi

---
+++ RazorPizza/Pages/PizzaSale.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizza.Pages
+{
+    public class PizzaSaleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
