e95750ca1ebe5275d81341fa4d65932453627ca9
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-06 09:31:10 +0300

    Custom Pizza page view tamamlandı

--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Model;
 
 namespace RazorPizza.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public Pizza Pizza { get; set; }
+
+
         public void OnGet()
         {
         }
