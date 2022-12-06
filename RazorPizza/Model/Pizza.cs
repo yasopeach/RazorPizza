4f1470ed8352330c943667466f9d20c5409ced0f
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-06 11:33:24 +0300

    Pizza model genişletildi

--- RazorPizza/Model/Pizza.cs
+++ RazorPizza/Model/Pizza.cs
@@ -8,5 +8,11 @@
         public bool Sauce { get; set; }
         public bool Cheese { get; set; }
         public bool Pepperoni { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Tuna { get; set; }
+        public bool Pineapple { get; set; }
+        public bool Ham { get; set; }
+        public bool Beef { get; set; }
+        public float FinalPrice { get; set; }
     }
 }
