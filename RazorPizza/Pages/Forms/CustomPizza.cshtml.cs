5f45cbf150064cad0bd5b8f9f3f1f2b26de2e277
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-06 09:48:54 +0300

    Custom Pizza page model tamamlandı

--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,10 +8,24 @@ namespace RazorPizza.Pages.Forms
     {
         [BindProperty]
         public Pizza Pizza { get; set; }
+        public float PizzaPrice { get; set; }
 
 
         public void OnGet()
         {
+        }
+
+
+        public IActionResult OnPost() // ActionResult, IActionResult, RedirectToPageResult = 3'ü de ayný anlama geliyor.
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            PizzaPrice += Pizza.Sauce ? 5 : 0;  // if pizza sauce seçilmiþ ise 5, seçilmemiþ ise pizzaprice'a 0 ekle
+            PizzaPrice += Pizza.Cheese ? 5 : 0;
+            PizzaPrice += Pizza.Pepperoni ? 5 : 0;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.Name, PizzaPrice } );
+
         }
     }
 }
