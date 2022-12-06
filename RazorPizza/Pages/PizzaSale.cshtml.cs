607aa23e0d065b97068f045bda2e3298dbb94cf9
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-06 12:24:57 +0300

    FakeDb (sahte/geçici) eklendi, PizzaSale page view düzenlendi

--- RazorPizza/Pages/PizzaSale.cshtml.cs
+++ RazorPizza/Pages/PizzaSale.cshtml.cs
@@ -1,10 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Model;
 
 namespace RazorPizza.Pages
 {
     public class PizzaSaleModel : PageModel
     {
+        public List<Pizza> FakePizzaDb = new List<Pizza>
+        {
+            new Pizza {
+                ImageTitle="Margerita",
+                Name="Margerita",
+                Sauce=true,
+            Cheese=true,
+            FinalPrice=4
+            },
+            new Pizza {
+                ImageTitle = "Bolognese",
+                Name = "Bolognese",
+                Sauce = true,
+                Cheese = true,
+                Beef = true,
+                FinalPrice = 5
+            },
+            new Pizza {
+                ImageTitle="Carbonara",
+                Name="Carbonara",
+                Sauce=true,
+                Cheese=true,
+                Ham=true,
+                FinalPrice=5
+            },
+            new Pizza {
+                ImageTitle="Hawaiian",
+                Name="Hawaiian",
+                Sauce=true,
+                Cheese=true,
+                Pepperoni=true,
+                Pineapple=true,
+                FinalPrice=6
+            },
+            new Pizza {
+                ImageTitle="MeatFeast",
+                Name="MeatFeast",
+                Sauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new Pizza {
+                ImageTitle="Mushroom",
+                Name="Mushroom",
+                Sauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new Pizza {
+                ImageTitle="Pepperoni",
+                Name="Pepperoni",
+                Sauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new Pizza {
+                ImageTitle="Seafood",
+                Name="Seafood",
+                Sauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new Pizza {
+                ImageTitle="Vegetarian",
+                Name="Vegetarian",
+                Sauce=true,
+                Cheese=true,
+                FinalPrice=4
+            }
+        };
+
         public void OnGet()
         {
         }
