8eb820db5b9f8f57ecb12e494e2594c2a78505f8
Canberk Ciftci <canberkciftci07@hotmail.com>
2022-12-06 11:29:03 +0300

    Checkout Page model tamamlandı, Pizza Sale sayfasının başlığı düzenlendi

--- RazorPizza/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizza.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string Name { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "Custom Pizza";
+            }
+
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
         }
     }
 }
